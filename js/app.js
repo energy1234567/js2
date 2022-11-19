0172cf048352c8779dfa302a085025ca7febcce7
energy1234567 <113248425+energy1234567@users.noreply.github.com>
2022-11-19 18:50:45 +0500

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,223 @@
+// let gog = 5;
+// gog = 2;
+// gog;
+
+// console.log(gog)
+
+// let a=4;
+//     b=7;
+//     c=11;
+//     console.log(a,b,c)
+
+    // let a = 4;
+    // let b = 6;
+    // c=9;
+    // console.log(c)
+
+   
+    // war can = 5;
+    // console.log(can)
+
+// var her = "ее";
+// console.log(her);
+
+// {
+//   let a = 5;
+//   console.log(a)
+// }
+// console.log(a)
+
+
+// const red = #000;
+// const blue = #135;
+
+// console.log(blue)
+// console.log(blue)
+// console.log(blue)
+// console.log(blue)
+// console.log(blue)
+// console.log(blue)
+// console.log(blue)
+// console.log(blue)
+// console.log(blue)
+// console.log(blue)
+// console.log(blue)
+// console.log(blue)
+// console.log(blue)
+// console.log(blue)
+// console.log(blue)
+// console.log(blue)
+// console.log(blue)
+// console.log(blue)
+// console.log(blue)
+// console.log(blue)
+// console.log(blue)
+// console.log(blue)
+// console.log(blue)
+
+   // переменная, в которой хранится выбранное математическое действие
+// функция расчёта
+// переменная для результата
+// получаем первое и второе число
+// смотрим, что было в переменной с действием, и действуем исходя из этого
+// отправляем результат на страницу
+
+
+
+
+
+// let admin, name;
+// name = "джон";
+// admin = name; 
+//  alert( admin );            
+
+
+// let ourPlanetName = "земля";
+// let ourPlanetName = "Земля";
+
+// let isBoss = confirm("Ты здесь главный?");
+
+// alert( isBoss ); // true, если нажата OK
+
+
+// let test = prompt("ваше имя?");
+
+
+// let name = prompt('как тебя зовут?');
+// alert (`тебя зовут  ${name} !`);
+
+
+// let username = prompt ('какой твой никнаме');
+// alert (`твой никнаме ${username} !`);
+
+// let x = 5;
+// x = -x;
+// alert(x);
+
+// let x=10, y=4;
+// alert ( x - y );
+
+// alert( 8 ** (1/3) );
+// let x = 10;
+// alert (-10);
+
+// alert ( +" ");
+// let apples=7;
+// let banana=10;
+// alert ( +apples + +banana ); 
+
+
+//  let x = 2  2 * 7;
+//  alert( x );
+
+
+// let a = 10;
+// let b = 20;
+// let c = 3 - (a = b + 2);
+// alert (a);
+// alert (c);
+
+
+// let n = 10 , a = 2;
+// a /= 2;
+// alert (a);
+
+// let n = 11;
+
+// n *= 4 + 7;
+// alert (n); 121
+
+// let counter = -7;
+//  let a = counter++; 
+// alert(a);
+
+// let a=0;
+// b =
+// --a
+// +
+// a--
+// +
+// ++a
+// -
+// a++;
+// b 
+
+
+// let a = 1 ;
+// let c = 2;
+
+// b = a++ - --a + --c + c++ * ++c + a-- / ++c; 
+
+
+// let a = 0;
+// let b = false;
+// console.log(a === b);
+
+// let name = prompt("Кто там?", '');
+
+
+
+
+// let name = prompt("Кто там?", '');
+
+// if (name === 'admin') {
+
+//   let pass = prompt('Пароль?', '');
+//   if (pass === '12345') {
+//      alert( 'e salom uzbellaga!' );
+//    } else if (pass === '' || pass === null) {
+//      alert( 'otmena ku yana, sassiq' );
+//    } else {
+//      alert( 'fu' );
+//    }
+
+//  } else if (name === '' || name === null) {
+//    alert( 'otmen buldi' );
+//  } else {
+//    alert( "admin mas san, toba" );
+//  }
+
+
+
+// let x = 2  2 * 7;
+//   alert( x );
+
+// if ( age < 5) {
+// message = 'hello';  
+// }else if (age < 20){
+// message = 'привет ';
+// } else if (age < 80){
+// message = 'здраствуйте'}
+
+// else {
+//   message = 
+// }
+
+
+// let a = 2; 
+// let x = 5 + (a *= 2 + (a += 4) + a++); 
+// alert (a)
+
+
+//  alert ("" - 1 + 0 );
+
+
+let name = prompt(" введи логин", '');
+
+if (name === 'admin') {
+
+  let pass = prompt('Пароль?', '');
+
+  if (pass === '12345') {
+    alert( 'e salom uzbellaga!' );
+  } else if (pass === '' || pass === null) {
+    alert( 'otmena ku yana, sassiq' );
+  } else {
+    alert( 'fu' );
+  }
+
+} else if (name === '' || name === null) {
+  alert( 'otmen buldi' );
+} else {
+  alert( "admin mas san, toba" );
+}
